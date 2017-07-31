2cef59579d8ff7d402f34bdf021cfcc40e7b2f48
kanontang <kanontang@gmail.com>
2017-07-31 08:12:35 +0800

    增加一个python工具

---
+++ table/xml2json.py
@@ -0,0 +1,36 @@
+#! /usr/bin/env python
+#coding=utf-8
+from xml.etree import ElementTree as et
+import json
+import os
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+print sys.getdefaultencoding()
+def xml2json(path):
+    filesList = os.listdir(path)
+    for file in filesList:
+        filename = str(file)
+        #忽略.开头的文件 ~开头的文件
+        if filename.startswith(".") or filename.startswith("~"):
+            continue
+        #只取.xlsm文件
+        if filename.endswith("xml") == False:
+            continue
+        #print "filename: " + path + filename
+        root = et.parse(path + filename)
+        childNodeName = filename.split(".")[0]
+        #print childNodeName
+        nodeList = []
+        f = open(path + childNodeName + ".json", "w")  
+        for node in root.getiterator(childNodeName):
+            nodeList.append(node.attrib)
+        tempJson = json.dumps(nodeList, ensure_ascii=False)
+        print tempJson
+        f.write(tempJson + "\n")
+        f.close() 
+
+#获取当前目录
+d = os.getcwd() + '\\cfg\\'
+print "run in document: " + d 
+xml2json(str(d))
